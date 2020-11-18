41c0bb794aab8e2383efbce0eca5667ac306a59b
JamesYang980323 <jameshy@g.ucla.edu>
2020-11-17 17:06:53 -0800

    get done with the screen setup and draw snake

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import pygame
 import sys
+import random
 
 # global variable
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
+
+# snake class
+class Snake(object):
+    def __init__(self, x, y):
+        self.init_pos_x=x
+        self.init_pos_y=y
+        self.dirc=pygame.K_RIGHT
 
 
 def main():
@@ -15,6 +23,12 @@ def main():
     snake_background = pygame.image.load('snakebackground.png')
     pygame.display.set_icon(snake_img)   # set the game Icon
     screen.blit(snake_background, [0, 0])   # set the background image
+    # instantiate a snake with random position
+    ran_x = random.randint(100, 500)
+    ran_y = random.randint(100,500)
+    snake = Snake(ran_x, ran_y)
+    # draw snake
+    pygame.draw.rect(screen, (255,69,0), [ran_x, ran_y, 100, 20])
 
     # keep the screen running until the user quit
     running = True
